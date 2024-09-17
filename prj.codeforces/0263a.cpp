be8a59518d3d292463892b83c69a18ccc659fc59
kiyotaka137 <rsainov@yandex.ru>
2024-09-17 22:24:02 +0300

    Update and rename 263a.cpp to 0263a.cpp

---
+++ prj.codeforces/0263a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+int main()
+{
+    const int n = 5;
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            std::cin >> a[i][j];
+            if (a[i][j] == 1) {
+                std::cout << abs(i - 2) + abs(j - 2);
+            }
+        }
+    } 
